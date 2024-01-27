d387e2732b7b91636e0348cb2997287bbf647c09
rezakariimii <rezakariimii1997@gmail.com>
2024-01-27 11:34:26 +0330

    01-Create-Next-App-Layout

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Amazon Shop",
+  description: "shopping website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>Copyright &copy; 2024 | Developer : Reza karimi</p>
+          </footer>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ components/header/Header.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Amazon
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link href="/cart" className="btn btn-ghost rounded-btn">
+                Cart
+              </Link>
+              <Link href="/signin" className="btn btn-ghost rounded-btn">
+                Sign in
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
